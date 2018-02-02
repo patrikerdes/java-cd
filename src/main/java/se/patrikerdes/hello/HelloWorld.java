3c9e15b0bc13015a31e7fa126aa7476df290066e
Patrik Erdes <patrik@erdes.se>
2018-02-02 13:51:19 +0100

    Initial commit

---
+++ src/main/java/se/patrikerdes/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package se.patrikerdes.hello;
+
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        get("/", (req, res) -> "Hello world!");
+    }
+}
