71f4b8fb1931a92ec01a465da88bb04a7ddcc4ed
Patrik Erdes <patrik@erdes.se>
2018-02-02 17:21:40 +0100

    Added failing test

---
+++ src/test/java/se/patrikerdes/hello/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package se.patrikerdes.hello;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    void testTest() {
+        assertEquals(true, false);
+    }
+}
