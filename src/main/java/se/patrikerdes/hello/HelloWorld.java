bcf21eae4f736b8ecfb68550922e8386b731f3b5
Patrik Erdes <patrik@erdes.se>
2018-02-02 14:39:56 +0100

    Make the port configurable (Heroku needs it)

--- src/main/java/se/patrikerdes/hello/HelloWorld.java
+++ src/main/java/se/patrikerdes/hello/HelloWorld.java
@@ -4,6 +4,7 @@ import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        port(Integer.parseInt(System.getenv("PORT")));
         get("/", (req, res) -> "Hello world!");
     }
 }
